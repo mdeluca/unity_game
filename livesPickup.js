0af84c7428a4c74c9d5a1508ee477d9b5485e511
Marco De Luca <m_deluca3@fanshaweonline.ca>
2013-11-09 19:26:32 -0500

    added health

--- livesPickup.js
+++ livesPickup.js
@@ -1,11 +1,13 @@
 #pragma strict
 
 public var lives : int = 5;
+public var health : int = 25;
 
 function OnTriggerEnter(other: Collider)
 {
 var playerStatus : mainPlayer = other.GetComponent(mainPlayer); //mainPlayer is the name of the script file
 if(playerStatus == null) return; 
 playerStatus.lives += lives;
+playerStatus.health += health;
 Destroy(gameObject); //destroy gameobject
 }
